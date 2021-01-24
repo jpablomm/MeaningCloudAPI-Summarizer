da169cd4aa9a8cc69812a355d283f73341cce664
Jose Pablo Moreno <josepablomorenomontes@Joses-MacBook-Pro.local>
2021-01-23 17:42:52 -0800

    Removed unnecessary code

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,28 +3,15 @@ import { postData } from './postData';
 import { updateUI } from './updateUI';
 
 
-// Weather API
-// const base_url = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-// const api_key = '&appid=5d10671a156bc50f016005850c1b873a&units=metric';
-
-// meaninn cloud API
-const BASE_API_URL = 'http://api.meaningcloud.com/summarization-1.0?key=';
-const API = 'bf085169b57e36115f7a5c37685392de';
-
 function handleSubmit(event) {
     event.preventDefault();
 
     let formText = document.getElementById('name').value;
-    console.log(formText);
-    console.log("::: Form Submitted :::");
     Client.getSummary('http://localhost:8081/api_call', formText)
         .then((data)=> {
-            console.log(data);
-            console.log(data.summary);
             Client.postData('http://localhost:8081/add', data.summary); 
         })
         .then(() => {
-            console.log('Updating UI');
             Client.updateUI();
         });
 };
--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,6 @@ var path = require('path');
 var https = require('follow-redirects').https;
 var fs = require('fs');
 const express = require('express');
-const mockAPIResponse = require('./mockAPI.js');
 const axios = require("axios");
 
 const dotenv = require('dotenv');
@@ -35,7 +34,6 @@ app.get('/', function (req, res) {
 
 app.get('/all', (req, res) => {
     res.send(projectData);
-    console.log(projectData);
 });
 
 // designates what port the app will listen to for incoming requests
@@ -43,79 +41,16 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 });
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-});
-
-// API call
-/* app.post('/api_call', (req, res) => {
-    console.log(req.body.url);
-    const user_url = req.body.url;
-
-    var options = {
-        'method': 'POST',
-        'hostname': 'api.meaningcloud.com',
-        'path': '/summarization-1.0?key=' + process.env.API_KEY + '&txt=' + user_url + '&sentences=5',
-        'headers': {
-        },
-        'maxRedirects': 20
-      };
-    
-    var req1 = https.request(options, function (res) {
-        var chunks = [];
-        console.log('chunks 1: ', chunks);
-        
-        res.on("data", function (chunk) {
-            console.log(chunk);
-            chunks.push(chunk);
-        });
-
-        console.log('chunks 2: ', chunks);
-
-        res.on("end", function (chunk) {
-          var body = Buffer.concat(chunks);
-          console.log(body.toString());
-          return body.toString();
-        });
-    
-        res.on("error", function (error) {
-          console.error(error);
-        });
-      });
-    res.send(req1);
-    req1.end();
-    
-}); */
 
 // POST route to add temperature, date, user response
 app.post('/add', (req, res) => {
-    console.log("Starting post request");
-    // console.log(req);
-    console.log(req.body);
     const newEntry = {
         summary: req.body,
     };
-    console.log(newEntry);
     projectData = newEntry;
     res.send(projectData);
-    console.log(projectData);
 });
 
-console.log(`Your API key is ${process.env.API_KEY}`);
-
-const getSummary = async (text) => {
-    const newURL = BASE_API_URL + process.env.API_KEY + '&url=' + text + '&sentences=5';
-    console.log(newURL);
-    const res = await fetch(newURL);
-    console.log(res);
-    try {
-        const data = await res.json();
-        console.log(data);
-        return data;
-    } catch(error) {
-        console.log("error", error);
-    }
-};
 
 app.post('/api_call', async (req, res) => {
     const userInput = req.body;
@@ -130,12 +65,10 @@ app.post('/api_call', async (req, res) => {
 });
 
 const fetchAPI = async (data) => {
-    console.log("FetchAPI data input:", data);
     const res = await axios(
         BASE_API_URL + process.env.API_KEY + '&url=' + data.url + '&sentences=5'
     );
     try {
-        console.log("Fetch API res.data: ",res.data);
         return res.data;
     } catch (e) {
         console.log("Error",e)
